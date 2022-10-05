c2180336cf0f3bc846888b2c4df11cf9467f3833
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-10-05 12:34:45 +0200

    create MsgService and MsgServiceBT

---
+++ garden-controller/MsgServiceBT.h
@@ -0,0 +1,24 @@
+#ifndef __MSGSERVICEBT__
+#define __MSGSERVICEBT__
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "MsgService.h"
+
+class MsgServiceBT {
+    
+public: 
+  MsgServiceBT(int rxPin, int txPin);  
+  void init();  
+  bool isMsgAvailable();
+  Msg* receiveMsg();
+  bool sendMsg(Msg msg);
+
+private:
+  String content;
+  Msg* availableMsg;
+  SoftwareSerial* channel;
+  
+};
+
+#endif
