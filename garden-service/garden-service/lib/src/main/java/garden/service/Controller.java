9b48939d16d860810574cbb1c0bf4eacc0abae36
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-10-05 14:53:32 +0200

    add Controller

---
+++ garden-service/garden-service/lib/src/main/java/garden/service/Controller.java
@@ -0,0 +1,41 @@
+package garden.service;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import SerialCommChannel.SerialCommChannel;
+
+public class Controller implements ActionListener {
+
+	SerialCommChannel channel;
+	GUI view;
+	Random rnd = new Random();
+	int temperature;
+	int intensity;
+
+	public Controller(String port, GUI view, LogView logger) throws Exception {
+		this.view = view;
+		channel = new SerialCommChannel(port, 9600);
+		new MonitoringAgent(channel,view,logger).start();
+
+		System.out.println("Waiting arduino for rebootin...");
+		
+		System.out.println("Ready");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			temperature = rnd.nextInt(30);
+			intensity = rnd.nextInt(100);
+			channel.sendMsg(Integer.toString(temperature));
+			channel.sendMsg(Integer.toString(intensity));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+
+
+}
