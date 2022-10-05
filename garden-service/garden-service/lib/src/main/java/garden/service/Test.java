7fa020a27117c0d570d7f5a337c1129935ddb766
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-10-05 14:27:47 +0200

    add CommChannel

---
+++ garden-service/garden-service/lib/src/main/java/garden/service/Test.java
@@ -0,0 +1,12 @@
+package garden.service;
+
+import io.vertx.core.AbstractVerticle;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
