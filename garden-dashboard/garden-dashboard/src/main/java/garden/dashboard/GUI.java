d7564144ca1f3b8f4c6d831149f2fdc277e3d994
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-10-10 10:00:25 +0200

    create new garden service and move gui to garden dashboard

---
+++ garden-dashboard/garden-dashboard/src/main/java/garden/dashboard/GUI.java
@@ -0,0 +1,30 @@
+package garden.dashboard;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame {
+	
+	private final JButton random = new JButton("Random");
+	
+	public GUI() {
+		
+		final JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setSize(450,450);
+		frame.setTitle("Garden Service");
+		
+		final JPanel panel = new JPanel();
+		
+		frame.getContentPane().add(panel);
+		
+		panel.add(random);
+		
+		frame.setVisible(true);
+	}
+
+	void registerController(Controller contr) {
+		random.addActionListener(contr);
+	}
+}
